e6c85aa3c8710a975d5c4ab541fbe0092a62ca30
Chase Boggess <chaseboggess92@yahoo.com>
2024-01-28 20:58:01 -0600

    test

--- resources/sqlDB/mysql.sql
+++ resources/sqlDB/mysql.sql
@@ -1,1 +1,7 @@
 CREATE TABLE movie(movieid INTEGER NOT NULL PRIMARY KEY, moviename TEXT NOT NULL, RATING DECIMAL(2,2))
+
+INSERT INTO movie(movieid, moviename, rating) VALUES (1,"Raging Bull", 10)
+
+SELECT * FROM movie
+
+DROP TABLE movie
